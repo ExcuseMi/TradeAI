e052c2c9ee2b865759b75b55ac364bb788f22e54
ExcuseMi <wardje@gmail.com>
2017-12-07 22:37:04 +0100

    Messing around

---
+++ TradeAI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Tasharen;
+using System.Collections;
+
+public class TradeAI : MonoBehaviour
+{
+   IEnumerator Start()
+   {
+      
+       for (; ; )
+       {
+            if(MyPlayer.ship != null) { 
+                if(GameShip.list.Count < 10) {
+                    string name = "Satan " + GameShip.list.Count;
+                    GameShip.CreateRandom("Satan " + GameShip.list.Count, MyPlayer.ship.position, 1f);
+                    UIStatusBar.Show("Created " + name);
+                    yield return new WaitForFixedUpdate();
+
+                }
+                else
+                {
+                    yield return UIStatusBar.Show("Full");
+                }
+            } else
+            {
+                yield return UIStatusBar.Show("Not ingame");
+            }
+        }
+   }
+
+}
+
---
+++ TradeAIShip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class TradeAIShip : GameShip
+{
+    public TradeAIShip()
+    {
+        this.name = "Satan";
+    }
+
+    protected void OnFollow(int id, bool canRepair)
+    {
+
+    }
+    protected void OnNavigate(Vector3 pos, bool active, int priority)
+    {
+
+    }
+    public void SayRandomEncouragement(GameShip target, float duration)
+    {
+
+    }
+
+}
